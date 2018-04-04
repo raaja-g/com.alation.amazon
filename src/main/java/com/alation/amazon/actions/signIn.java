8702b4d0c30f2067bd0e7e6f8d7ab853786d010c
rajag <rajag@192.168.0.100>
2018-04-05 02:02:42 +0530

    Necessary Comments added.

--- src/main/java/com/alation/amazon/actions/signIn.java
+++ src/main/java/com/alation/amazon/actions/signIn.java
@@ -4,13 +4,15 @@ import com.alation.amazon.page.*;
 
 public class signIn {
 
+	// We perform Login action here.
+
 	public void performLogin(String userName, String password) {
 		loginPage login = new loginPage();
 		login.setuserName(userName);
 		login.performcontinue();
 		login.setpassword(password);
 		login.performsignIn();
-		
+
 	}
 
 }
