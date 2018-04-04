d610aa1b78e239feeb72f6134b1fb7bb395b6858
rajag <rajag@192.168.0.100>
2018-04-05 01:42:05 +0530

    Comments added.

--- src/main/java/com/alation/amazon/page/productSearchPage.java
+++ src/main/java/com/alation/amazon/page/productSearchPage.java
@@ -6,6 +6,9 @@ import com.alation.amazon.scripts.browserAction;
 
 public class productSearchPage extends browserAction {
 
+	// All the objects related to the page.
+	// Any changes to the object we change it here.
+
 	private final By selectCategory = By.id("searchDropdownBox");
 	private final By selectCategoryOption = By.cssSelector("option[value=\"search-alias=stripbooks\"]");
 	private final By searchProduct = By.name("field-keywords");
@@ -28,46 +31,64 @@ public class productSearchPage extends browserAction {
 	public productSearchPage() {
 	}
 
+	// Selects the category "Books" in the selection.
+
 	public void selectCategory(String category) throws Exception {
 		select(selectCategory, selectCategoryOption, category);
 	}
+
+	// Enters the value "Data Catalog" in the search bar.
+	// This is parameterized.
+	// We can send the value from the driver script @Test
 
 	public void searchProduct(String bookName) {
 		sendKeys(searchProduct, bookName);
 	}
 
+	// Clicks the Search button.
 	public void performSearchGo() {
 		click(performSearch);
 	}
 
+	// We get the First Book Name.
 	public String getfirstBookName() {
 		return getElementText(firstBookName);
 	}
 
+	// We get the First Book's Author Name.
 	public String getfirstBookAuthor() {
 		return getElementText(firstBookAuthor);
 	}
 
+	// We get the First Book's Edition Type.
 	public String getfirstBookEditionType() {
 		return getElementText(firstBookEditionType);
 	}
 
+	// We get the First Book's Rent Price (Total value and not whole or Fractional
+	// value).
 	public String getfirstBookRentPrice() {
 		return getElementText(firstBookRentPrice);
 	}
 
+	// We get the First Book's Buy Price (Total value and not whole or Fractional
+	// value).
 	public String getfirstBookBuyPrice() {
 		return getElementText(firstBookBuyPrice);
 	}
 
+	// We get the First Book's Total stocks left
 	public String getfirstBookTotalStocksLeft() {
 		return getElementText(firstBookTotalStocksLeft);
 	}
 
+	// We get the First Book's Kindle Price (Total value and not whole or Fractional
+	// value).
 	public String getfirstBookKindlePrice() {
 		return getElementText(firstBookKindlePrice);
 	}
 
+	// We use this to confirm the selected category
 	public String getconfirmCategory() {
 		return getElementText(confirmCategory);
 	}
