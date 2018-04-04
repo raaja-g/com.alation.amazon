3e78b211342b74862630ab2f9eb93e29f216bfa1
rajag <rajag@192.168.0.100>
2018-04-05 01:45:13 +0530

    Added Necessary comments and checked-in

--- src/main/java/com/alation/amazon/page/loginPage.java
+++ src/main/java/com/alation/amazon/page/loginPage.java
@@ -6,6 +6,9 @@ import com.alation.amazon.scripts.browserAction;
 
 public class loginPage extends browserAction {
 
+	// All the objects related to the page.
+	// Any changes to the object we change it here.
+
 	private final By userName = By.xpath("//*[@id='ap_email']");
 	private final By password = By.xpath("//*[@id='ap_password']");
 	private final By continueButton = By.xpath("//*[@id='continue']/span");
@@ -14,17 +17,24 @@ public class loginPage extends browserAction {
 	public loginPage() {
 	}
 
+	// We enter Username with this method.
+
 	public void setuserName(String username) {
 		sendKeys(userName, username);
 	}
 
+	// We enter Password with this method.
+
 	public void setpassword(String Password) {
 		sendKeys(password, Password);
 	}
+	// We Click on Continue button with this method.
 
 	public void performcontinue() {
 		click(continueButton);
 	}
+
+	// We Click on Sign In button with this method.
 
 	public void performsignIn() {
 		click(signInButton);
